fe1aec6750a6493abfce5c142d5abed48402acc8
z3db0y <us3r0002@gmail.com>
2022-09-14 19:40:59 +0300

    RunMode holdPosition fix

--- TeamCode/src/main/java/com/z3db0y/davidlib/Motor.java
+++ TeamCode/src/main/java/com/z3db0y/davidlib/Motor.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.DcMotorImplEx;
 public class Motor extends DcMotorImplEx {
     boolean hold = false;
     double power = 0;
+    RunMode runMode = RunMode.RUN_WITHOUT_ENCODER;
 
     public Motor(DcMotorImplEx motor) {
         super(motor.getController(), motor.getPortNumber());
@@ -27,6 +28,16 @@ public class Motor extends DcMotorImplEx {
         this.power = power;
         super.setPower(power);
         holdPosition();
+    }
+
+    public void setMode(RunMode runMode) {
+        this.runMode = runMode;
+        super.setMode(runMode);
+        holdPosition();
+    }
+
+    public RunMode getMode() {
+        return this.runMode;
     }
 
     public double getPower() {
